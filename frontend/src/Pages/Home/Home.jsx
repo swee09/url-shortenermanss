e35c8099b5a747854d8ddde5fa612f49148e882d
Manasvi Vongur <2310030267@klh.edu.in>
2026-05-11 13:34:15 +0530

    Update Home.jsx

--- frontend/src/Pages/Home/Home.jsx
+++ frontend/src/Pages/Home/Home.jsx
@@ -53,7 +53,7 @@ const Home = () => {
               marginTop: "1rem",
             }}
           >
-            Your App Name
+            Manswee's Url shorterner
           </Title>
 
           <Text
